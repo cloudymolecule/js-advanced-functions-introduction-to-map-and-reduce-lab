af8b71e524d678a773ead081a0d713ed8db8a5a7
Cloudymolecule <baires82@gmail.com>
2021-02-26 10:47:03 -0500

    commit tree 3d1a59eb64fe76b96543465905ead271e4f6cd8a

--- index.js
+++ index.js
@@ -1,1 +1,62 @@
-// Your code here
+// map-like methods
+
+function mapToNegativize(array){
+    let newArray = []
+    array.forEach(element => {
+        newArray.push(element - (element * 2))
+    })
+    return newArray
+}
+
+function mapToNoChange(array){
+    let newArray = []
+    array.forEach(element => {
+        newArray.push(element)
+    })
+    return newArray
+}
+
+function mapToDouble(array){
+    let newArray = []
+    array.forEach(element => {
+        newArray.push(element * 2)
+    })
+    return newArray
+}
+
+function mapToSquare(array){
+    let newArray = []
+    array.forEach(element => {
+        newArray.push(Math.pow(element, 2))
+    })
+    return newArray
+}
+
+
+// reduce-like methods
+
+function reduceToTotal(array, startingPoint){
+    let total
+    if (startingPoint) {total = startingPoint} else {total = 0}
+    array.forEach(element => {
+        total = total + element
+    })
+    return total
+}
+
+function reduceToAllTrue(array){
+    let res = true
+    array.forEach(element => {
+        if (!element) {res = false}
+    })
+    return res
+}
+
+function reduceToAnyTrue(array){
+    let res = false
+    array.forEach(element => {
+        if (!!element) {res = true}
+    })
+    return res
+}
+
